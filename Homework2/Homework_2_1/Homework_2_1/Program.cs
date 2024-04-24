de190283e76ac8b86fdd02a4a59c05d3b96c5056
adimitrovska <adimitrovska@tu-sofia.bg>
2024-04-25 02:59:33 +0300

    Added Task1

---
+++ Homework2/Homework_2_1/Homework_2_1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework_2_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			ThreeDPointWithMass point1 = new ThreeDPointWithMass();
+			point1.X = 1;
+			point1.Y = 2;
+			point1.Z = 3;
+			point1.Mass = 4;
+
+			ThreeDPointWithMass point2 = new ThreeDPointWithMass();
+			point2.X = 5;
+			point2.Y = 6;
+			point2.Z = 7;
+			point2.Mass = -8;
+			ThreeDPointWithMass point3 = new ThreeDPointWithMass();
+			point3.X = 0;
+			point3.Y = 0;
+			point3.Z = 0;
+			point3.Mass = 12;
+
+            Console.WriteLine($"Cordinates for Point1: X = {point1.X}  Y = {point1.Y}  Z = {point1.Z} Mass: {point1.Mass}");
+			Console.WriteLine($"Cordinates for Point2: X = {point2.X}  Y = {point2.Y}  Z = {point2.Z} Mass: {point2.Mass}");
+			Console.WriteLine($"Cordinates for Point3: X = {point3.X}  Y = {point3.Y}  Z = {point3.Z} Mass: {point3.Mass}");
+			Console.WriteLine();
+
+            Console.WriteLine($"Distance between Point1 and Point2 is: {point1.DistanceTo(point2)}");
+			Console.WriteLine($"Distance between Point1 and Point3 is: {point1.DistanceTo(point3)}");
+			Console.WriteLine($"Distance between Point2 and Point3 is: {point2.DistanceTo(point3)}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Is Point1 at coordinates (0, 0, 0) ? {point1.IsZero()}");
+			Console.WriteLine($"Is Point2 at coordinates (0, 0, 0) ? {point2.IsZero()}");
+			Console.WriteLine($"Is Point3 at coordinates (0, 0, 0) ? {point3.IsZero()}");
+		}
+	}
+}
---
+++ Homework2/Homework_2_1/Homework_2_1/ThreeDPointWithMass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2_1
+{
+	internal class ThreeDPointWithMass
+	{
+		private int[] _3D = new int[3];
+		private double mass;
+		public int X
+		{
+			get { return _3D[0]; }
+			set { _3D[0] = value; }
+		}
+		public int Y
+		{
+			get { return _3D[1]; }
+			set { _3D[1] = value; }
+		}
+		public int Z
+		{
+			get { return _3D[2]; }
+			set { _3D[2] = value; }
+		}
+		public double Mass
+		{
+			get
+			{ return mass; }
+			set
+			{
+				if (value > 0)
+				{
+					mass = value;
+				}
+				else
+					mass = 0;
+			}
+		}
+		
+		public bool IsZero()
+		{
+			if(X==0 && Y==0 && Z== 0)
+			{
+				return true;
+			}
+			else 
+				return false;
+		}
+
+		public double DistanceTo(ThreeDPointWithMass obj)
+		{
+			int x2=obj.X;
+			int y2=obj.Y;
+			int z2=obj.Z;
+
+			float distanceX = X-x2;
+			float distanceY = Y- y2;
+			float distanceZ = Z -z2;
+
+			double finalResult= Math.Sqrt(Math.Pow(distanceX,2) + Math.Pow(distanceY,2) + Math.Pow(distanceZ,2));
+
+			return finalResult;
+		}
+	}
+}
